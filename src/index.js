8607037647c6283070e43571c124ea71fd1380ed
Andy Tan <andytan0727@gmail.com>
2020-04-04 15:53:32 +0800

    optimize build for packaging
    Using UMD build as main file instead of ./src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import vCurrencySelect from "./CurrencySelect";
 
-export default {
-  install(Vue) {
-    // register component globally
-    Vue.component("v-currency-select", vCurrencySelect);
-  }
+// Declare install function executed by Vue.use()
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+
+  // register component globally
+  Vue.component("v-currency-select", vCurrencySelect);
+}
+
+// Create module definition for Vue.use()
+const plugin = {
+  install
 };
 
-export { vCurrencySelect };
+// Auto-install when vue is found (eg. in browser via <script> tag)
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+vCurrencySelect.install = install;
+
+// To allow use as module (npm/webpack/etc.) export component
+export default vCurrencySelect;
